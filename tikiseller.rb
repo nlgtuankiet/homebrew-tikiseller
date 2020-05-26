1cb7481735876263ffdcf5e740331524ba15d709
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2020-05-26 13:04:10 +0700

    version 1.2

--- tikiseller.rb
+++ tikiseller.rb
@@ -1,11 +1,12 @@
 class Tikiseller < Formula
   desc "A command line tool for tiki seller"
   homepage "https://github.com/nlgtuankiet/tiki-seller-cli"
-  url "https://github.com/nlgtuankiet/tiki-seller-cli/releases/download/1.1/tikiseller.zip"
-  sha256 "0431c0eade60c0d9e351b954ec99042e86f41f2a82daf5fbc84d13680dc24389"
-  version "1.0.0"
+  url "https://github.com/nlgtuankiet/tiki-seller-cli/releases/download/1.2/tikiseller.zip"
+  sha256 "0029f17d92fc5c4ac7ec87ad6fe2d23e6e57fff8643e3fb74e6272b771bcc0dc"
+  version "1.2.0"
 
   depends_on "ffmpeg"
+  depends_on "java"
 
   bottle :unneeded
 
